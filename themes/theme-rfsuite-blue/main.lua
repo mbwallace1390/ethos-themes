7c098d0ebb6b1a9b26543669ae1e37eb84f4b311
Michael Wallace <mbwallace1390@gmail.com>
2026-07-05 20:17:57 -0500

    Test sharper toolbar and blue-black gutters

--- themes/theme-rfsuite-blue/main.lua
+++ themes/theme-rfsuite-blue/main.lua
@@ -17,15 +17,15 @@ local function init()
             COLOR_BLACK,               -- OVERLAY_COLOR
             lcd.RGB(0xb8, 0xc3, 0xe4), -- SECONDARY_COLOR
             lcd.RGB(0x69, 0xFF, 0x94), -- SAFE_COLOR
-            lcd.RGB(0x19, 0x1A, 0x21), -- PAGE_BGCOLOR
+            lcd.RGB(0x0A, 0x0F, 0x19), -- PAGE_BGCOLOR (blue-black outer gutters)
             lcd.RGB(0xDE, 0x57, 0x35), -- ERROR_COLOR
             lcd.RGB(0x00, 0x78, 0xE8), -- ACTIVE_COLOR
             lcd.RGB(0xb8, 0xc3, 0xe4), -- INACTIVE_COLOR 0xFF, 0x80, 0xBF
             lcd.RGB(0x00, 0x78, 0xE8), -- BUTTON_BORDER_ACTIVE_COLOR
             lcd.RGB(0x50, 0x52, 0x61), -- BUTTON_BORDER_COLOR
             lcd.RGB(0xA3, 0x95, 0x14), -- WARNING_COLOR
-            lcd.RGB(0x19, 0x1A, 0x21), -- SAFE_CONTRASTING_COLOR
-            COLOR_BLACK,               -- TOPLCD_BGCOLOR (XE/S)
+            lcd.RGB(0x0A, 0x0F, 0x19), -- SAFE_CONTRASTING_COLOR
+            lcd.RGB(0x0A, 0x0F, 0x19), -- TOPLCD_BGCOLOR (XE/S)
         },
         --toolbarLogo = "none",
         toolbarBackground = lcd.loadBitmap("toolbar-rfsuite-blue.png"),
